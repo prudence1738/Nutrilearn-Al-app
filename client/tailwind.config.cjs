b48c64f14f4e97e67f7b75daca94f2371c355941
Prudence Oyando <prudenceoyando98@gmail.com>
2025-11-21 14:41:56 +0300

    Add Tailwind CSS configuration file

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        green900: '#064e3b',
+        green700: '#047857',
+        green500: '#10b981',
+        green300: '#86efac'
+      }
+    }
+  },
+  plugins: []
+};
